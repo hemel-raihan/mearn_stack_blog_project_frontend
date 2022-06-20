89b606fa75ec121fa3e6d701bd6987b6d012cd6e
hemelraihan <hemelraihan1997@gmail.com>
2022-06-21 00:54:02 +0600

    init commit

---
+++ src/topbar/TopBar.jsx
@@ -0,0 +1,17 @@
+import './topbar.css';
+
+export default function TopBar() {
+  return (
+    <div className='top'>
+      <div className="topLeft">
+l
+      </div>
+      <div className="topCenter">
+c
+      </div>
+      <div className="topRight">
+r
+      </div>
+    </div>
+  )
+}
