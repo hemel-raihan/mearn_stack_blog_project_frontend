424dab7f71576ec964d978db95c8afba6a0c6b94
hemelraihan <hemelraihan1997@gmail.com>
2022-06-21 23:43:26 +0600

    add navbar, header, sidebar in home

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -4,13 +4,23 @@ export default function TopBar() {
   return (
     <div className='top'>
       <div className="topLeft">
-l
+      <i className="topIcon fa-brands fa-facebook"></i>
+      <i className="topIcon fa-brands fa-twitter"></i>
+      <i className="topIcon fa-brands fa-pinterest"></i>
+      <i className="topIcon fa-brands fa-instagram"></i>
       </div>
       <div className="topCenter">
-c
+        <ul className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
       </div>
       <div className="topRight">
-r
+        <img className='topImg' src='https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='' />
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
       </div>
     </div>
   )
