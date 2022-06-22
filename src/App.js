28199253809f1e581802d283b1e7fe8eb53446be
hemelraihan <hemelraihan1997@gmail.com>
2022-06-23 00:33:54 +0600

    add post, singlePost, createPost, settings & login pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
 import Home from './pages/home/Home';
-import TopBar from './topbar/TopBar';
+import TopBar from './components/topbar/TopBar';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
 
 function App() {
   return (
